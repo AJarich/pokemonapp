ced6fa1418b3e8c926afbd8b34d9faca47eaf25c
Andrea Jarich <glenstone93@yahoo.com>
2022-06-17 21:48:30 -0500

    Edited PokemonList file Ran Npm run build to build project so it can be ran live.

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -7,13 +7,18 @@ export default function PokemonList({pokemon}){
         <div class="col-sm-3 my-2 p-2">
         <div class="card">
           <div class={p.types[0].type.name}>
-            <img class="card-top-img" src={p.sprites.front_shiny}/>
+            <img class="card-top-img" src={p.sprites.front_shiny} alt={p.name}/>
           </div>
           <div class="card-body" key={p.name}>
             <span class="card-title pokemon-name"><h5 >{p.id}. {p.name.charAt(0).toUpperCase() + p.name.slice(1)}</h5></span>
             <span class="card-text">Type: {p.types[0].type.name.charAt(0).toUpperCase() + p.types[0].type.name.slice(1)}</span><br />
             <span class="card-text">Height: {p.height * 10} cm</span><br />
-            <span class="card-text">Weight: {p.weight * 0.1} kg</span><br />
+            <span class="card-text">Weight: {(p.weight * 0.1).toFixed(2)} kg</span><br />
+            <span class="card-text">HP: {p.stats[0].base_stat}</span><br />
+            <span class="card-text">Attack: {p.stats[1].base_stat}</span><br />
+            <span class="card-text">Defense: {p.stats[2].base_stat}</span><br />
+            <span class="card-text">Abilities: {p.abilities[0].ability.name.charAt(0).toUpperCase() + p.abilities[0].ability.name.slice(1)}
+              {p.abilities[1]?.ability.name ? ', ' + p.abilities[1].ability.name.charAt(0).toUpperCase() + p.abilities[1].ability.name.slice(1) : ''}</span>
           </div>
         </div>
       </div>
